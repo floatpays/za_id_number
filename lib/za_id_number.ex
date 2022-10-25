7934aba62c7e47ce85644cbbdf8eb7de61e752d9
Wilhelm Hugo Kirschbaum <wkirschbaum@gmail.com>
2022-10-25 08:47:34 +0200

    Generate mix project

---
+++ lib/za_id_number.ex
@@ -0,0 +1,18 @@
+defmodule ZaIdNumber do
+  @moduledoc """
+  Documentation for `ZaIdNumber`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ZaIdNumber.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
